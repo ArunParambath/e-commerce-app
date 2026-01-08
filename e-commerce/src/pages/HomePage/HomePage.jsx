fef0cd14d3b08b9ea352d63df97b775a2c76865d
Arun Parambath <arunparambath11@gmail.com>
2026-01-08 16:09:51 +0530

    Fetch data from backend

--- e-commerce/src/pages/HomePage/HomePage.jsx
+++ e-commerce/src/pages/HomePage/HomePage.jsx
@@ -5,8 +5,14 @@ import './HomePage.css';
 
 
 export function HomePage() {
-   fetch('')
-   
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+            console.log(data);
+        });
+
+
     return (
         <>
             <Header />
